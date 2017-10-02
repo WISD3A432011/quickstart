72a228d3bda2a5a66987fbcf8146c4e7609b0477
CHIA-YU TSAI <scorpio1106vt@gm.student.ncut.edu.tw>
2017-10-02 18:10:52 +0800

    add new task

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,17 @@ use Illuminate\Http\Request;
      * 增加新的任務
      */
     Route::post('/task', function (Request $request) {
-        //
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:255',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect('/')
+                ->withInput()
+                ->withErrors($validator);
+        }
+
+        // 建立該任務..
     });
 
     /**
