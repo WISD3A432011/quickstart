64351952d309d2f40de49a6ba4fbdac260275f0e
CHIA-YU TSAI <scorpio1106vt@gm.student.ncut.edu.tw>
2017-10-02 18:18:11 +0800

    update web.php(顯示已有的任務清單)

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,11 @@ use Illuminate\Http\Request;
      */
     Route::get('/', function () {
         //
-        return view('tasks');
+        $tasks = Task::orderBy('created_at', 'asc')->get();
+
+        return view('tasks', [
+            'tasks' => $tasks
+        ]);
     });
 
     /**
